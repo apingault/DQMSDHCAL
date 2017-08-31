58f749b9d2724a4df9effc139d8202397347be2f
Antoine Pingault <antoine.pingault@gmail.com>
2017-08-31 09:50:07 +0200

    Fix crash when filling rateVsCluster Profile

--- source/modules/HitAnalysisModule/HitAnalysisModule.cc
+++ source/modules/HitAnalysisModule/HitAnalysisModule.cc
@@ -425,9 +425,10 @@ dqm4hep::StatusCode HitAnalysisModule::processEvent(EVENT::LCEvent *pLCEvent)
 		// Fill Number Of Clusters vs Rate
 		// if (m_eventParameters.spillIntegratedTime > 0 && m_pEventClassifier->isPhysicsEvent() && m_pEventClassifier->getEventType() != EventClassifier::COSMIC_MUON_EVENT)
 		if (m_eventParameters.spillIntegratedTime > 0 && m_pEventClassifier->isPhysicsEvent())
-			m_pRateVsClusterProfile->get<TProfile>()->Fill(m_nParticleWithinSpill / m_eventParameters.lastSpillIntegratedTime * m_pEventHelper->getDAQ_BC_Period(), clusters.size());
-		if (m_eventParameters.spillIntegratedTime > 0)
-			m_pRateVsClusterProfileNoClassification->get<TProfile>()->Fill(m_nParticleWithinSpill / m_eventParameters.lastSpillIntegratedTime * m_pEventHelper->getDAQ_BC_Period(), clusters.size());
+		    m_pRateVsClusterProfile->get<TProfile>()->Fill(m_nParticleWithinSpill / m_eventParameters.spillIntegratedTime * m_pEventHelper->getDAQ_BC_Period(), clusters.size());
+
+		// if (m_eventParameters.spillIntegratedTime > 0)
+		// 	m_pRateVsClusterProfileNoClassification->get<TProfile>()->Fill(m_nParticleWithinSpill / m_eventParameters.lastSpillIntegratedTime * m_pEventHelper->getDAQ_BC_Period(), clusters.size());
 
 		int nHit0 = 0;
 		int nHit1 = 0;
